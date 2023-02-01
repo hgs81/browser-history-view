337ab3d63ceb7b20844974ca1183fe256ed90a1d
hgs81 <hgs88801@gmail.com>
2023-02-02 08:40:24 +0900

    update params.py

--- params.py
+++ params.py
@@ -8,3 +8,22 @@ exclude_domains = [
     'localhost',
     '.local',
 ]
+exclude_files = [
+    # '*cjpalhdlnbpafiamejdnhcphjbkeiagm*',
+    '*Extensions/cjpalhdlnbpafiamejdnhcphjbkeiagm*',    # uBlockOrigin
+    '*Extensions/hdokiejnpimakedhajhdlcegeplioahd*',    # LastPass
+    '*Extensions/nkbihfbeogaeaoehlefnkodbefgpgknn*',    # MetaMask
+    '*Extensions/kbfnbcaeplbcioakkpcpgfkobkghlhen*',    # Grammarly
+    '*Extensions/liecbddmkiiihnedobmlmillhodjkdmb*',    # Loom – Screen Recorder & Screen Capture
+    '*Extensions/laookkfknpbbblfpciffpaejjkokdgca*',    # Momentum
+    '*Extensions/lpcaedmchfhocbbapmcbpinfpgnhiddi*',    # Google Keep
+    '*IndexedDB/*.hopanatech.com*',
+    '*IndexedDB/*.google.com*',
+    '*IndexedDB/*.codesignal.com*',
+    '*IndexedDB/*.diagrams.net*',
+    '*IndexedDB/*.csb.app*',
+    '*IndexedDB/*.webex.com*',
+    '*IndexedDB/*.zoom.us*',
+    '*IndexedDB/https_zoom.us*',
+    '*IndexedDB/https_playcode.io*',
+]
